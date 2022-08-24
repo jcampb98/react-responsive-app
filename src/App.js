fbab8c83419ee74d85f06850ba10df948f4a44e5
jcampb98 <caboose768@gmail.com>
2022-08-24 13:56:14 +0100

    created assets folder, components folder to be used in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ScrollToTop from './components/ScrollToTop'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Recommend from './components/Recommend'
+import Footer from './components/Footer'
+import Testimonial from './components/Testimonial'
+import Services from './components/Services'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <ScrollToTop />
+    <Navbar />
+    <Hero />
+    <Services />
+    <Recommend />
+    <Testimonial />
+    <Footer />
+    </>
+  )
 }
-
-export default App;
